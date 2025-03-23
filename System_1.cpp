76eadd2a6e8e7e32888128b688b21c2a0ab52525
user-1 <user-1@user-1-VirtualBox>
2025-03-23 10:28:56 +0000

    rebasing

---
+++ System_1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "config.h"
+
+int main(int argc, char **argv) {
+	std::cout << "Hello World" << std::endl;
+	std::cout << "Version " << System_1_VERSION_MAJOR << "." << System_1_VERSION_MINOR << std::endl;
+	return 0; 
+}
